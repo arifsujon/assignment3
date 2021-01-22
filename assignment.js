7345394c8c5b4fe2e516485b51d26d704a5bfb9d
arifsujon <50014257+arifsujon@users.noreply.github.com>
2021-01-22 23:41:14 +0600

    Update assignment.js

--- assignment.js
+++ assignment.js
@@ -1,45 +1,61 @@
+// Problem No 1: kilometerToMeter
+
 function kilometerToMeter(kilometer) {
-    var meter = kilometer * 1000;
-    return meter;
+    var meter = kilometer * 1000;  // 1 kilometer = 1000 meters
+    if (kilometer > 0) {
+        return meter;
+    } else {
+        return "The distance can't be 0 or negative value";
+    }
 }
-var homeToSchool = kilometerToMeter(3);
-console.log(homeToSchool);
-var homeToPlayground = kilometerToMeter(1);
-console.log(homeToPlayground);
-var homeToMarket = kilometerToMeter(2);
-console.log(homeToMarket); 
 
-
+// Problem No 2: budgetCalculator
 
 function budgetCalculator(watch, phone, laptop) {
-    var watchBuyingCost = watch * 50;
-    var phoneBuyingCost = phone * 100;
-    var laptopBuyingCost = laptop * 500;
+    var watchBuyingCost = watch * 50;    // The price of each watch is 50.
+    var phoneBuyingCost = phone * 100;   // The price of each phone is 100.
+    var laptopBuyingCost = laptop * 500; // The price of each laptop is 500.
     totalBuyingCost = watchBuyingCost + phoneBuyingCost + laptopBuyingCost;
-    console.log(totalBuyingCost);
-    return totalBuyingCost;
+    if (watch >= 0 && phone >= 0 && laptop >= 0) {
+        return totalBuyingCost;
+    } else {
+        return "watch/phone/laptop amount is/are not negative.";
+    }
 }
 
+// Problem No 3: hotelCost
 
-function hotelCost(stayDays){
+function hotelCost(stayDays) {
     var livingCost = 0;
-    if(stayDays <= 10){
-        livingCost = stayDays * 100;
-    }
-    else if(stayDays <= 20){
+    if (stayDays <= 10) {
+        livingCost = stayDays * 100;               //100 per day living cost for firstTenDays
+    } else if (stayDays <= 20) {
         var firstTenDays = 10 * 100;
         var remainingDays = stayDays - 10;
-        var secondTenDays = remainingDays * 80;
+        var secondTenDays = remainingDays * 80;     //80 per day living cost for secondTenDays
         livingCost = firstTenDays + secondTenDays;
-    }
-    else{
+    } else {
         var firstTenDays = 10 * 100;
         var secondTenDays = 10 * 80;
         var remainingDays = stayDays - 20;
-        var afterTwentyDays = remainingDays * 50;
+        var afterTwentyDays = remainingDays * 50;    //50 per day living cost afterTwentyDays
         livingCost = firstTenDays + secondTenDays + afterTwentyDays;
     }
     return livingCost;
+}
+
+// Problem No 4: megaFriend
+
+var friendsName = ["Rahim", "Karim", "Rashed", "Nazrul"];
+function megaFriend(friendsName) {
+    var megaName = friendsName[0];
+    for (var i = 0; i < friendsName.length; i++) {
+        var element = friendsName[i];
+        if (element.length > megaName.length) {
+            megaName = element;
+        }
+    }
+    return megaName;
 }
 
 
